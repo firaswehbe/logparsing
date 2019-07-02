8560b048c9d4c1ea1b367ed5910db1b05a6819d1
Firas Wehbe <firaswehbe@users.noreply.github.com>
2019-07-01 19:40:52 -0500

    Expand R parsing to include stats per hour

--- parse_parsed.R
+++ parse_parsed.R
@@ -8,11 +8,19 @@ mydata = read.csv('output/parsed.csv', stringsAsFactors = FALSE,
     datetime_p = as.POSIXct(datetime,format='%d/%b/%Y:%T %z'),
     year_p = strftime(datetime_p,'%Y'),
     month_p = strftime(datetime_p,'%m'),
-    date_p = strftime(datetime_p,'%d')
+    date_p = strftime(datetime_p,'%d'),
+    hour_p = strftime(datetime_p,'%H')
     )
 
-mysummary = mydata %>% select(year_p,month_p,date_p,ip) %>%
+mysummary_day = mydata %>% select(year_p,month_p,date_p,ip) %>%
   group_by(year_p,month_p,date_p) %>%
   summarise(hits = n(), hits_unique_ip = n_distinct(ip))
 
-write.csv(mysummary,'output/summary.csv',row.names = FALSE)
+write.csv(mysummary_day,'output/summary_day.csv',row.names = FALSE)
+
+mysummary_hour = mydata %>% select(year_p,month_p,date_p,hour_p,ip) %>%
+  group_by(year_p,month_p,date_p,hour_p) %>%
+  summarise(hits = n(), hits_unique_ip = n_distinct(ip)) %>% 
+  arrange(year_p,month_p,date_p,hour_p)
+  
+write.csv(mysummary_hour,'output/summary_hour.csv',row.names = FALSE)
